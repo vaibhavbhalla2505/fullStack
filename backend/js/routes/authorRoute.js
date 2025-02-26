e4e8dc26ad9c16bb5d2d901865407750e4635385
vaibhav bhalla <vaibhav.bhalla@sourcefuse.com>
2025-02-26 15:24:32 +0530

    adding "js folder into gitignore"

--- backend/js/routes/authorRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createAuthor, deleteAuthor, getAllAuthor, updateAuthor } from "../controller/authorController.js";
-const router = express.Router();
-router.post('/create-author', createAuthor);
-router.get('/getAllAuthor', getAllAuthor);
-router.put('/update-author/:id', updateAuthor);
-router.delete('/delete-author/:id', deleteAuthor);
-export default router;
--- backend/js/routes/bookRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createBook, deleteBookDetails, getAllBooks, updateBookDetails } from '../controller/bookController.js';
-const router = express.Router();
-router.post('/create-book', createBook);
-router.get('/getAllBooks', getAllBooks);
-router.put('/update-book/:id', updateBookDetails);
-router.delete('/delete-book/:id', deleteBookDetails);
-export default router;
